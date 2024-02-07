edcaad26acf8d349dd3535a09a2994f794753972
danielml <daniel@danielml.dev>
2024-02-07 12:48:35 +0100

    display-drivers: Import Xiaomi changes from bsp-vermeer-t-oss

--- qcom/opensource/display-drivers/msm/msm_drv.h
+++ qcom/opensource/display-drivers/msm/msm_drv.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
  * Copyright (c) 2016-2021, The Linux Foundation. All rights reserved.
  * Copyright (C) 2013 Red Hat
  * Author: Rob Clark <robdclark@gmail.com>
@@ -186,7 +186,6 @@ enum msm_mdp_crtc_property {
 	CRTC_PROP_VM_REQ_STATE,
 	CRTC_PROP_NOISE_LAYER_V1,
 	CRTC_PROP_FRAME_DATA_BUF,
-
 	/* total # of properties */
 	CRTC_PROP_COUNT
 };
@@ -238,7 +237,9 @@ enum msm_mdp_conn_property {
 	CONNECTOR_PROP_CACHE_STATE,
 	CONNECTOR_PROP_DSC_MODE,
 	CONNECTOR_PROP_WB_USAGE_TYPE,
-
+	/* mi add crtc property */
+	CONNECTOR_PROP_MI_LAYER_INFO,
+	CONNECTOR_PROP_QSYNC_MIN_FPS_INDEX,
 	/* total # of properties */
 	CONNECTOR_PROP_COUNT
 };
@@ -875,6 +876,7 @@ struct msm_resource_caps_info {
  * @display_type:       Enum for type of display
  * @is_te_using_watchdog_timer:  Boolean to indicate watchdog TE is
  *				 used instead of panel TE in cmd mode panels
+ * @switch_vsync_delay: Boolean to indicate whether panel requires extra vsync during fps switch
  * @poms_align_vsync:   poms with vsync aligned
  * @roi_caps:           Region of interest capability info
  * @qsync_min_fps	Minimum fps supported by Qsync feature
@@ -904,6 +906,7 @@ struct msm_display_info {
 
 	uint32_t display_type;
 	bool is_te_using_watchdog_timer;
+	bool switch_vsync_delay;
 	bool poms_align_vsync;
 	struct msm_roi_caps roi_caps;
 
@@ -946,6 +949,7 @@ struct msm_display_kickoff_params {
 struct msm_display_conn_params {
 	uint32_t qsync_mode;
 	bool qsync_update;
+	uint32_t qsync_min_fps_index;
 };
 
 /**
@@ -1093,6 +1097,8 @@ struct msm_drm_private {
 	struct mutex vm_client_lock;
 	struct list_head vm_client_list;
 };
+
+struct drm_connector_state *_msm_get_conn_state(struct drm_crtc_state *crtc_state);
 
 /* get struct msm_kms * from drm_device * */
 #define ddev_to_msm_kms(D) ((D) && (D)->dev_private ? \
@@ -1279,9 +1285,6 @@ int msm_framebuffer_set_cache_hint(struct drm_framebuffer *fb,
 		u32 flags, u32 rd_type, u32 wr_type);
 int msm_framebuffer_get_cache_hint(struct drm_framebuffer *fb,
 		u32 *flags, u32 *rd_type, u32 *wr_type);
-
-int msm_fb_obj_get_attrs(struct drm_gem_object *obj,
-		int *fb_ns, int *fb_sec, int *fb_sec_dir);
 
 struct drm_fb_helper *msm_fbdev_init(struct drm_device *dev);
 void msm_fbdev_free(struct drm_device *dev);
